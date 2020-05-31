75d77ef093dd01dcea45c3af00f2de15f0383873
onigra <3280467rec@gmail.com>
2020-06-01 00:20:37 +0900

    set return type

--- src/messageBuilder.ts
+++ src/messageBuilder.ts
@@ -3,7 +3,7 @@ import { color } from "./color";
 import { titleBlocks } from "./titleBlocks";
 import { fields } from "./fields";
 
-function build(event: any) {
+function build(event: any): MessageAttachment {
   const message: MessageAttachment = {
     color: color(event),
     blocks: titleBlocks(),
