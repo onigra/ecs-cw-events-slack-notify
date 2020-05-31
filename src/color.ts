b589ee156537ce309ad09bfcd663dd8392b76f71
onigra <3280467rec@gmail.com>
2020-05-31 19:34:16 +0900

    add color test

--- src/color.ts
+++ src/color.ts
@@ -1,3 +1,14 @@
+const normalStatus: string[] = ["PENDING"];
+const warningStatus: string[] = ["STOPPED"];
+
 export function color(event: any): string {
+  if (normalStatus.includes(event.detail.lastStatus)) {
+    return "#3498DB";
+  }
+
+  if (warningStatus.includes(event.detail.lastStatus)) {
+    return "warning";
+  }
+
   return "good";
 }
