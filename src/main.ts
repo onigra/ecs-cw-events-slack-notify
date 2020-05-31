31c2a23c79a200973ad2292cb87b5051101cdc41
onigra <3280467rec@gmail.com>
2020-05-31 20:17:25 +0900

    debug log

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const url = process.env.SLACK_WEBHOOK_URL;
 const webhook = new IncomingWebhook(url);
 
 export const handler = async (event: any) => {
+  console.log(event);
   const message = buildMessage(event);
   await webhook.send(message);
   return "ok";
