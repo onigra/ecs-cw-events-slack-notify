2f3139c70a069da36942dcbb21e6de355c59cdc2
onigra <3280467rec@gmail.com>
2020-05-31 17:49:50 +0900

    modify handler

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
+import { IncomingWebhook } from "@slack/webhook";
+import { buildMessage } from "./messageBuilder";
+
+const url = process.env.SLACK_WEBHOOK_URL;
+const webhook = new IncomingWebhook(url);
+
 export const handler = async (event: any) => {
+  const message = buildMessage(event);
+  await webhook.send(message);
   return "ok";
 };
