5eb53a8eb8ac1def7ff6ccc4dc49ae8e8f712d4d
onigra <3280467rec@gmail.com>
2020-06-01 15:25:11 +0900

    add PROVISIONING color

--- src/color.ts
+++ src/color.ts
@@ -1,4 +1,4 @@
-const normalStatus: string[] = ["PENDING"];
+const normalStatus: string[] = ["PROVISIONING", "PENDING"];
 const warningStatus: string[] = ["STOPPED"];
 
 export function color(event: any): string {
