02c7de2ae3bdaa8a2630f193b47ef9f0001dbbd6
onigra <3280467rec@gmail.com>
2020-06-02 23:14:20 +0900

    mofidy field test

--- test/fields.test.ts
+++ test/fields.test.ts
@@ -2,22 +2,56 @@ import { fields } from "../src/fields";
 
 import { running } from "./data/running";
 
-test("fho", async () => {
-  expect(fields(running)).toStrictEqual([
+test("running event", async () => {
+  // setup
+  const cluster = "test-cluster";
+  const service = "test-service";
+  const taskDef = "test-task-def:10";
+  const deploymentStatus = "RUNNING";
+  const taskId = "9999";
+  const tasks = [
+    "*Status:* RUNNING *Task:* app *ImageTag:* test-image:test-tag",
+    "*Status:* RUNNING *Task:* sidecar *ImageTag:* sidecar:latest",
+  ].join("\n");
+
+  //and
+  const expected = [
     {
       title: "Cluster",
-      value: "cluster1",
+      value: cluster,
+      short: true,
+    },
+    {
+      title: "Service",
+      value: service,
       short: true,
     },
     {
       title: "Task Definition",
-      value: "testTD:1",
+      value: taskDef,
       short: true,
     },
     {
-      title: "Status",
-      value: "RUNNING",
+      title: "Deployment Status",
+      value: deploymentStatus,
       short: true,
     },
-  ]);
+    {
+      title: "Task Id",
+      value: taskId,
+      short: false,
+    },
+    {
+      title: "Tasks",
+      value: tasks,
+      short: false,
+    },
+  ];
+
+  // when
+  const actual = fields(running);
+
+  // then
+
+  expect(actual).toStrictEqual(expected);
 });
